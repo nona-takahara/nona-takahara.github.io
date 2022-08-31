d380dcee2314de59af635aa177c1e5861ee53887
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2022-09-01 05:01:14 +0900

    Display Styleの数式描画に対応

--- src/items/katex-load.ts
+++ src/items/katex-load.ts
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', (ev) => {
   const mathElements = document.querySelectorAll('.math');
   mathElements.forEach(element => {
-    element.innerHTML = katex.renderToString(element.innerHTML, {throwOnError: false});
+    element.innerHTML = katex.renderToString(element.innerHTML, {throwOnError: false, displayMode: element.classList.contains('math-display')});
   });
 });
