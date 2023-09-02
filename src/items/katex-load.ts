64f9853bfdb057af349a594af805b1aeb9ed36b0
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2023-09-02 15:14:06 +0900

    Astroでページ制作するための最小限の構成を実施

--- src/items/katex-load.ts
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', (ev) => {
-  const mathElements = document.querySelectorAll('.math');
-  mathElements.forEach(element => {
-    element.innerHTML = katex.renderToString(element.innerHTML, {throwOnError: false, displayMode: element.classList.contains('math-display')});
-  });
-});
