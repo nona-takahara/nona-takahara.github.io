241ee6ebcca20b36b0db664c0811453cc48ae505
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2023-09-03 15:56:07 +0900

    Feature change buildengine to astro (#7)
    * Astroでページ制作するための最小限の構成を実施
    
    * ブログ記事を吐き出せるように修正
    
    * すべてのページにレイアウトを適用
    
    * さらにシンプル化を推進
    
    * コメント欄の識別子を設定
    
    * ブログ機能の調整
    
    * 時間取得機能を追加
    
    * 再フォーマット
    
    * 詳細な動作の暫定対応とアクセシビリティ調整
    
    * ビルド設定周りを調整
    
    * 旧テンプレートの削除
    
    * KaTeXを条件ロードに変更
    
    * タイトルの変更に対応
    
    * ブログのタイトル部分の修正
    
    * デプロイ設定

---
+++ astro.config.mjs
@@ -0,0 +1,105 @@
+import { defineConfig } from 'astro/config';
+
+import { combineExtensions } from 'micromark-util-combine-extensions';
+import { gfmStrikethrough } from 'micromark-extension-gfm-strikethrough';
+import { gfmTable } from 'micromark-extension-gfm-table';
+
+import { gfmStrikethroughFromMarkdown, gfmStrikethroughToMarkdown } from 'mdast-util-gfm-strikethrough';
+import { gfmTableFromMarkdown, gfmTableToMarkdown } from 'mdast-util-gfm-table';
+import { visit } from 'unist-util-visit';
+import { is } from 'unist-util-is';
+
+import remarkMath from 'remark-math';
+
+function gfm(options) {
+    return combineExtensions([gfmStrikethrough(options), gfmTable]);
+}
+
+function gfmFromMarkdown() {
+    return [gfmStrikethroughFromMarkdown, gfmTableFromMarkdown];
+}
+
+function gfmToMarkdown(options) {
+    return ({
+        extensions: [gfmStrikethroughToMarkdown, gfmTableToMarkdown(options)]
+    });
+}
+
+function remarkGfm(options = {}) {
+    const data = this.data();
+    add('micromarkExtensions', gfm(options));
+    add('fromMarkdownExtensions', gfmFromMarkdown());
+    add('toMarkdownExtensions', gfmToMarkdown(options));
+    function add(field, value) {
+        const list = (data[field] ? data[field] : (data[field] = []));
+        list.push(value);
+    }
+}
+
+function underLine() {
+    return (tree, file) => {
+        visit(tree, 'strong', (node) => {
+            const startCode = node.children[0].position.start.offset - 2;
+            if (file.toString().substring(startCode, startCode + 2) === '__') {
+                node.data = { hName: 'u' };
+            }
+        });
+    }
+}
+
+function MDNblock() {
+    return (tree, file) => {
+        visit(tree, 'blockquote', (node) => {
+            const firstChildren = node?.children?.[0];
+            if (is(firstChildren, 'paragraph') && is(firstChildren?.children?.[0], 'strong')) {
+                const boxType = firstChildren.children[0].children[0].value;
+
+                if (boxType.includes("Key")) {
+                    petitcomKey(node);
+                }
+            }
+        });
+    }
+}
+
+function petitcomKey(node) {
+    const valueString = node.children[0].children?.[1]?.value?.split(/\((.+)\)/);
+    const keyTarget = valueString?.[1];
+    const keyValue = valueString?.[2];
+
+    node.type = 'columnbox';
+    node.data = {
+        hProperties: {
+            className: ['petitcom-key']
+        },
+        hChildren: [
+            {
+                type: 'element',
+                tagName: 'span',
+                properties: { className: ['key-target'] },
+                children: [{ type: 'text', value: keyTarget }]
+            },
+            {
+                type: 'element',
+                tagName: 'span',
+                properties: { className: ['key'] },
+                children: [{ type: 'text', value: keyValue }]
+            }
+        ]
+    };
+    node.children = [];
+}
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://nona-takahara.github.io',
+    build: {
+        format: 'file'
+    },
+    markdown: {
+        remarkPlugins: [
+            remarkGfm, remarkMath, underLine, MDNblock
+        ],
+        syntaxHighlight: false
+    }
+});
