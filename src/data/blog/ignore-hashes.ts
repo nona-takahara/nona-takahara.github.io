f520e5a1594eeb5e94d886389e4799de7e049133
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2024-12-05 22:45:01 +0900

    更新日時について、無視するハッシュを追加

--- src/data/blog/ignore-hashes.ts
+++ src/data/blog/ignore-hashes.ts
@@ -1,3 +1,4 @@
 export default [
-  "768f061aee9aa80e258a0516c55347b01aed5c80", // dataディレクトリをcontentディレクトリにリネーム
+  "768f061aee9aa80e258a0516c55347b01aed5c80", // dataディレクトリをcontentディレクトリにリネーム（Astro v?対応）
+  "44332fca9c394ee6dc2b651e28a91c92a2804521", // contentディレクトリをdataディレクトリにリネーム（Astro v5対応）
 ];
