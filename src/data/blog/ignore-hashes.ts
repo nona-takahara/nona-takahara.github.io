81a0dfa9efff66cf9034f6060a60ee1cab0833a0
Nona Takahara <nona_t_2612@yahoo.co.jp>
2026-02-24 22:28:45 +0900

    Update ignore-hashes

--- src/data/blog/ignore-hashes.ts
+++ src/data/blog/ignore-hashes.ts
@@ -2,4 +2,6 @@ export default [
   "768f061aee9aa80e258a0516c55347b01aed5c80", // dataディレクトリをcontentディレクトリにリネーム（Astro v?対応）
   "6063540c70f66a38d0e3a07cc15b5b06558215a8", // contentディレクトリをdataディレクトリにリネーム（Astro v5対応）
   "6766445395324b23a7b5970b562d19baf10946b7", // MarkdownからMarkdown + JSXに変更にあたり、.mdxにリネーム（Park UI化）
+  "dce4b55d49c71123b1da3dd3ffe05d06b6b614b4", // MDX文法に修正
+  "8a95cdf81f3ed6e30c00ea43702bd8abf109d831", // Rubyタグの修正
 ];
